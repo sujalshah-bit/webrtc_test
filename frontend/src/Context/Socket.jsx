73114d3e0512a7e1787bb898e08d85d7c21b3099
sujal <sujalshah28092004@gmail.com>
2024-10-02 20:11:58 +0530

    change url

--- frontend/src/Context/Socket.jsx
+++ frontend/src/Context/Socket.jsx
@@ -10,7 +10,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("http://localhost:5000"), []);
+  const socket = useMemo(() => io("https://testsujalvideocallfrontendapi.vercel.app/"), []);
   return (
     <SocketContext.Provider value={socket}>
       {props.children}
